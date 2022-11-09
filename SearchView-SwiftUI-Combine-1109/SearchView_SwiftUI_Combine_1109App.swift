93e73752963eb01a4623b174e96c74b74e82dd22
Chris <kimjm010@icloud.com>
2022-11-10 00:37:52 +0900

    Initial Commit

---
+++ SearchView-SwiftUI-Combine-1109/SearchView_SwiftUI_Combine_1109App.swift
@@ -0,0 +1,17 @@
+//
+//  SearchView_SwiftUI_Combine_1109App.swift
+//  SearchView-SwiftUI-Combine-1109
+//
+//  Created by Chris Kim on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchView_SwiftUI_Combine_1109App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
